b705fa060f92fc4a198c234fd6f9a5cf19620330
Ott Toomet <otoomet@gmail.com>
2023-01-29 17:07:08 -0800

    lab3, very basic data frames

---
+++ lab.R
@@ -0,0 +1,121 @@
+### -------------------- Lab 3 --------------------
+### This lab is designed to prepare you for PS3
+
+## Create a vector of the number of points the Seahawks scored in the
+## first (at least) 5 games
+## Hint: google "Seahawks scores", or check the football database:
+## http://www.footballdb.com/teams/nfl/seattle-seahawks/results
+## here 'Final' displays scores
+## Use an appropriate variable name for the scores
+## 
+## Hint: feel free to invent if you cannot figure this out
+
+
+## Create a vector of the number of points the opponent
+## scored against Seahawks in the first 5 games
+## use an appropriate variable name
+
+
+## Combine your two vectors into a dataframe
+
+
+## Create a new column "diff" that is the difference in points
+## (in favor of Hawks)
+
+
+## Create a new column "won" which is TRUE if the Seahawks won,
+## ie if Seahawks scored more than the opponent scored against them
+
+
+## Create a vector of the opponents name (such as "Denver Broncos")
+
+
+## Add the vector of opponents into the data frame
+
+
+## Compute the average score of Seahawks
+
+
+## Compute how many games did Seahawks won
+## (use the 'won' variable to compute it)
+
+
+## What was the largest difference in scores (in favor of Seahawks)?
+
+
+## How many different opponents did Seahawks have in these games?
+## Hint: use `unique()` and `length()`
+
+
+## Print the variable names in your data frame
+
+
+## Write a loop over all variables in your data frame
+## print the variable name inside the loop
+
+
+## Write a loop over all variables in your data frame
+## print the variable name inside the loop,
+## and true/false, depending if the variable is numeric
+## check out 'is.numeric()'
+
+
+## Write a loop over all variables in your data frame
+## print the variable name inside the loop,
+## and the average value of the variable
+## if the variable is numeric
+
+
+##
+## HR data
+## You are working in HR for a large firm with 100 employees.
+## You are analyzing their salaries.
+
+## Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee
+## 100")
+## Hint: use 'paste()` or `str_c`
+
+
+## Create a random vector of their 2021 salaries.
+## Hint: you may use the runif function to create uniform random numbers,
+## e.g. 'runif(100, 60, 120)' creates 100 random numbers between 60 and 120
+
+
+## Create a random vector of 2022 salaries that are typically
+## higher than the 2014 salaires (use runif again).
+## For instance, if you create random numbers between 65 and 130, then 2022
+## salaries tend to be larger than 2021 salaries.
+
+
+## Create a data.frame 'salaries' by combining the vectors you just made
+
+
+## Create a column 'raise' that stores the size of the
+## raise between 2021 and 2022
+
+
+## Retrieve values from your data frame to answer the following questions:
+##
+## What was the 2015 salary of employee 57
+
+
+## Now round the answer down to two digits after comma
+## check out 'round()' function
+
+
+## How many employees got a raise?
+
+
+## What was the value of the highest raise?
+## Round the number to two digits!
+
+
+## What was the name of the employee who recieved the highest raise?
+
+
+## What was the average salary increase?
+## Round the number!
+
+
+## For people who did not get a raise, how much money did they lose?
+## Round the number!
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Lab 3
+
+This lab covers basic data frames and data-frame related methods.
+
+It is targeting some of the tricks in PS3
